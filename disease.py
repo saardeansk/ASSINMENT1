04c242f10f448e5be3d7e6be2765691e0d7b1c99
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-10-28 19:09:06 +0530

    Update disease.py

--- disease.py
+++ disease.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import datetime
-image_path = 'leaf.jpg'  # replace with  your leaf image
+image_path = 'leaf.jpg'   # replace with  your leaf image
 image = cv2.imread(image_path)
 
 if image is None:
